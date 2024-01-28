9c6b7754082cc795a14da84290ff5e97b5a0480f
tian yuan <tianyuan@192.168.1.105>
2024-01-28 17:26:12 +0000

    styling for single page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <nav class="mb-4">
-  <a href="{{ route('tasks.create') }}" class="font-medium text-gray-700 underline decoration-pink-500">Add Task</a>
+  <a href="{{ route('tasks.create') }}" class="link">Add Task</a>
 </nav>
 @forelse($tasks as $task)
   <div><a href="{{route('tasks.show',['task'=>$task->id])}}" @class(['line-through'=>$task->completed])>{{$task->title}}</a></div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,16 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  {{-- blade-formatter-disable --}}
+  <style type="text/tailwindcss">
+    .btn{
+      @apply rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 text-slate-700
+    }
+    .link{
+      @apply font-medium text-gray-700 underline decoration-pink-500
+    }
+  </style>
+  {{-- blade-formatter-enable --}}
   @yield('styles')
   <title>Task List APP</title>
    <script src="https://cdn.tailwindcss.com"></script>
